40c1fd9d41cfb2f419295d76e19d4c8e2aed015c
Brayan Jone <brayanjone01@gmail.com>
2026-06-16 19:57:22 -0300

    Create Alocacao.java

---
+++ Alocacao.java
@@ -0,0 +1,43 @@
+public class Alocacao {
+
+    private Disciplina disciplina;
+    private Professor professor;
+    private Horario horario;
+
+    public Alocacao(Disciplina disciplina,
+                    Professor professor,
+                    Horario horario) {
+
+        if (disciplina == null ||
+                professor == null ||
+                horario == null) {
+
+            throw new IllegalArgumentException("Dados inválidos.");
+        }
+
+        this.disciplina = disciplina;
+        this.professor = professor;
+        this.horario = horario;
+    }
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public Horario getHorario() {
+        return horario;
+    }
+
+    @Override
+    public String toString() {
+        return disciplina.getNome()
+                + " - "
+                + professor.getNome()
+                + " - "
+                + horario;
+    }
+}
