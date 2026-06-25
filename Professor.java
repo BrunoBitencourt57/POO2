5bb29d7d96c17c768d18a60ed244cd3efa948710
Brayan Jone <brayanjone01@gmail.com>
2026-06-25 03:00:51 -0300

    Update Professor.java
    Importei Collections, e usei collections.unmodifiableset nos gets competencias e disponibilidades, fiz validações nos metodos adicionarCompetencia e adicionarDisponibilidade, criei metodos possuiCompetencia e estaDisponivel

--- Professor.java
+++ Professor.java
@@ -1,24 +1,22 @@
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Professor {
 
     private String nome;
-
     private Set<String> competencias;
-
     private Set<Horario> disponibilidades;
 
     public Professor(String nome) {
 
         if (nome == null || nome.isBlank()) {
-            throw new IllegalArgumentException("Nome inválido");
+            throw new IllegalArgumentException("Nome inválido.");
         }
 
         this.nome = nome;
-
-        competencias = new HashSet<>();
-        disponibilidades = new HashSet<>();
+        this.competencias = new HashSet<>();
+        this.disponibilidades = new HashSet<>();
     }
 
     public String getNome() {
@@ -26,18 +24,39 @@ public class Professor {
     }
 
     public Set<String> getCompetencias() {
-        return competencias;
+        return Collections.unmodifiableSet(competencias);
     }
 
     public Set<Horario> getDisponibilidades() {
-        return disponibilidades;
+        return Collections.unmodifiableSet(disponibilidades);
     }
 
     public void adicionarCompetencia(String competencia) {
+        if (competencia == null || competencia.isBlank()) {
+            throw new IllegalArgumentException("Competência inválida.");
+        }
+
         competencias.add(competencia);
     }
 
     public void adicionarDisponibilidade(Horario horario) {
+        if (horario == null) {
+            throw new IllegalArgumentException("Horário inválido.");
+        }
+
         disponibilidades.add(horario);
+    }
+
+    public boolean possuiCompetencia(String competencia) {
+        return competencias.contains(competencia);
+    }
+
+    public boolean estaDisponivel(Horario horario) {
+        return disponibilidades.contains(horario);
+    }
+
+    @Override
+    public String toString() {
+        return nome;
     }
 }
